dda67826925e2d88748f869d08b062357b12cd45
NarooSister <dongeun49@naver.com>
2024-03-18 11:46:09 +0900

    init

---
+++ lol_likelion/src/main/java/com/example/lol_likelion/LolLikelionApplication.java
@@ -0,0 +1,13 @@
+package com.example.lol_likelion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolLikelionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolLikelionApplication.class, args);
+	}
+
+}
