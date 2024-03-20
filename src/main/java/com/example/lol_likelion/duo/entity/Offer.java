55d3aaba1297b036c670fc1fa531b5f842322002
kokiuk <kgukid38@naver.com>
2024-03-20 15:19:55 +0900

    git commit -m “[Feat] Offer and Post Entity , add @AllArgsConstructor”

--- src/main/java/com/example/lol_likelion/duo/entity/Offer.java
+++ src/main/java/com/example/lol_likelion/duo/entity/Offer.java
@@ -1,17 +1,18 @@
 package com.example.lol_likelion.duo.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Getter
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 public class Offer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/lol_likelion/duo/entity/Post.java
+++ src/main/java/com/example/lol_likelion/duo/entity/Post.java
@@ -1,6 +1,7 @@
 package com.example.lol_likelion.duo.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
