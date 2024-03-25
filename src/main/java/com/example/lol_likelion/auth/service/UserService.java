f2cfa372faaad6b5af3b8fb159ce36575b10fc90
NarooSister <dongeun49@naver.com>
2024-03-26 02:33:21 +0900

    [fix] jwt 수정에 따른 회원가입, 로그인, 마이페이지 기능 구현 및 화면 제작

--- src/main/java/com/example/lol_likelion/auth/service/UserService.java
+++ src/main/java/com/example/lol_likelion/auth/service/UserService.java
@@ -1,10 +1,11 @@
 package com.example.lol_likelion.auth.service;
 
+import com.example.lol_likelion.api.ApiService;
+import com.example.lol_likelion.api.dto.PuuidDto;
+import com.example.lol_likelion.api.dto.SummonerDto;
 import com.example.lol_likelion.auth.dto.JwtRequestDto;
-import com.example.lol_likelion.auth.dto.JwtResponseDto;
 import com.example.lol_likelion.auth.dto.CreateUserDto;
 import com.example.lol_likelion.auth.dto.CustomUserDetails;
-import com.example.lol_likelion.auth.dto.UpdateUserDto;
 import com.example.lol_likelion.auth.dto.UserInfoDto;
 import com.example.lol_likelion.auth.entity.UserEntity;
 import com.example.lol_likelion.auth.jwt.JwtTokenUtils;
@@ -15,8 +16,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -25,72 +26,64 @@ import java.util.Optional;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ApiService apiService;
     private final JwtTokenUtils jwtTokenUtils;
     private final AuthenticationFacade authenticationFacade;
 
-    @Transactional
+    //존재하는 아이디인지 확인
+    public boolean checkUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    //존재하는 소환사 닉네임인지 확인
+    public boolean checkGameName(String tagLine, String gameName){
+        return userRepository.existsByTagLineAndGameName(tagLine, gameName);
+    }
+
+    //Riot Api를 통해 실제로 있는 소환사 아이디인지 확인
+    public boolean riotApiCheckGameName(String tagLine, String gameName) {
+        //puuid 가져오기
+        PuuidDto puuidDto = apiService.callRiotApiPuuid(gameName, tagLine);
+        //puuid로 소환사 닉네임 가져오기
+        SummonerDto summonerDto = apiService.callRiotApiSummonerId(puuidDto);
+
+        return summonerDto.getId().equals(gameName);
+    }
+
+    //회원가입
     public void createUser(CreateUserDto dto) {
-        if (userRepository.existsByUsername(dto.getUsername()))
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "이미 존재하는 아이디 입니다.");
-
-        //TODO gameName 확인하기
-        //gameName과 tagLine으로 puuid 검색
-
-        //이미 있는 사용자인지 확인
-
-        //DB에 puuid 저장
-
-
-        UserInfoDto.fromEntity(userRepository.save(UserEntity.builder()
-                .username(dto.getUsername())
-                .password(passwordEncoder.encode(dto.getPassword()))
-                .tagLine(dto.getTagLine())
-                .gameName(dto.getGameName())
-                .email(dto.getEmail())
-                .phone(dto.getPhone())
-                .roles("ROLE_USER")
-                .build()));
+        userRepository.save(dto.toEntity(passwordEncoder.encode(dto.getPassword())));
     }
 
-    //jwt 토큰 발급
-    @Transactional
-    public void login(JwtRequestDto dto, HttpServletResponse response) {
-        UserEntity userEntity = userRepository.findByUsername(dto.getUsername())
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "유저 정보를 찾을 수 없습니다."));
+    //로그인
+    public UserEntity login(JwtRequestDto dto) {
+        Optional<UserEntity> optionalUser = userRepository.findByUsername(dto.getUsername());
+        if (optionalUser.isEmpty()) {
+            return null;
+        }
+        //비밀번호 확인
+        UserEntity user = optionalUser.get();
+        if (!passwordEncoder.matches(dto.getPassword(), user.getPassword())) {
+            return null;
+        }
 
-        if (!passwordEncoder.matches(dto.getPassword(), userEntity.getPassword()))
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "비밀번호가 일치하지 않습니다.");
-
-//        String jwt = jwtTokenUtils.generateToken(CustomUserDetails.fromEntity(userEntity));
-//        JwtResponseDto responseDto = new JwtResponseDto();
-//        responseDto.setToken(jwt);
-//        return responseDto;
-
-        CustomUserDetails userDetails = CustomUserDetails.fromEntity(userEntity);
-        // JWT 토큰 생성
-        String jwt = jwtTokenUtils.generateToken(userDetails);
-
-        // JWT 토큰을 쿠키에 저장
-        Cookie cookie = new Cookie("token", jwt);
-        //   cookie.setHttpOnly(true); // XSS 공격 방지
-       // cookie.setPath("/"); // 전체 경로에서 쿠키 사용
-        cookie.setMaxAge(60 * 60 * 60);
-        response.addCookie(cookie);
-
+        return user;
     }
 
-//    //READ one
-//    public UserInfoDto readUser(JwtRequestDto dto){
-//        UserEntity userEntity = userRepository.findByUsername(dto.getUsername())
-//                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "유저 정보를 찾을 수 없습니다."));
-//
-//        return UserInfoDto.fromEntity(userEntity);
-//    }
+    //my-page (구현중)
+    //username을 입력받아 UserEntity를 return
+    //인증, 인가에 사용 가능
+    public UserEntity getUserByUsername(String username) {
+        if (username == null) return null;
+        Optional<UserEntity> optionalUser = userRepository.findByUsername(username);
+        return optionalUser.orElse(null);
+    }
 
 //    @Transactional
 //    public UserInfoDto updateUser(UpdateUserDto dto) {
