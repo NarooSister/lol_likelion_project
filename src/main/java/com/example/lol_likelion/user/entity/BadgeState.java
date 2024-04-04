db6ce827fb1d605dcd362ba2dc135abe6f7231b8
kokiuk <kgukid38@naver.com>
2024-04-04 10:44:46 +0900

    [fix] pull받는 과정에서 전체 파일 빨간불이 떠서 다시 push 해봄

--- src/main/java/com/example/lol_likelion/user/entity/BadgeState.java
+++
@@ -1,8 +0,0 @@
-package com.example.lol_likelion.user.entity;
-
-public enum BadgeState {
-    NOT_ACQUIRED, // 미획득
-    ACQUIRED, // 획득
-    TRUST,  //신뢰 뱃지
-    REPRESENTATIVE // 대표 뱃지
-}
--- src/main/java/com/example/lol_likelion/user/repository/QuestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.lol_likelion.user.repository;
-
-import com.example.lol_likelion.user.entity.Quest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestRepository extends JpaRepository<Quest, Long> {
-}
